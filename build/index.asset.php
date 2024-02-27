27ada4845c606df91febd02805e23202640f7064
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2024-02-27 17:25:18 +0100

    First plugin commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '06c401270e5a27376c24');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '180d202864f77da9f2ad');
